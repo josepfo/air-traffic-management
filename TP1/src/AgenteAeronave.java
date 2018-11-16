fbf4bbf94a4daea12528eaab82c91304c4ba5b82
Carlos Campos <31832440+czgcampos@users.noreply.github.com>
2018-11-16 14:42:16 +0000

    Update AgenteAeronave.java

--- TP1/src/AgenteAeronave.java
+++ TP1/src/AgenteAeronave.java
@@ -6,30 +6,19 @@ import java.util.*;
 
 public class AgenteAeronave extends Agent{
 	
-	
-
 	private double coordX;
 	private double coordY;
-	
-	
 	private double destCoordX;
 	private double destCoordY;
-	
 	private double zonaProtegida;
 	private double zonaAlerta;
-	
 	private int nrPassageiros;
-	
-	
-	//private  direcao 
-	
+	private double direcaoX;
+	private double direcaoY;
 	private double velocidade;
 	private double distPercorrer;
 	private double distPercorrida;
 	
-
-	
-
 	@Override
 	protected void setup() {
 		
@@ -40,5 +29,7 @@ public class AgenteAeronave extends Agent{
 		
 	}
 
-
+	private class ReceberCoordsComp extends CyclicBeahaviour{
+		
+	}
 }
