f22b21e269c5ec2a536f34acddddf70c22dd4e91
Carlos Campos <31832440+czgcampos@users.noreply.github.com>
2018-11-24 15:11:18 +0000

    AE Final
    Agente Estação Final

--- TP1/src/AgenteEstacao.java
+++ TP1/src/AgenteEstacao.java
@@ -1,5 +1,6 @@
-import java.util.*;
+package Agents;
 
+import java.util.*;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
@@ -10,258 +11,139 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class AgenteEstacao extends Agent{
-
 	private double coordX;
 	private double coordY;
-	
 	private int totalPistas;
 	private int nrPistasOcupadas;
 	private int totalEstacionamentos;
 	private int nrEstOcupados;
-	
 	private double zonaProtegida;
 	private double zonaAlerta;
-	
-	// condições meteorologicas favoraveis ou não
+	// condiÃ§Ãµes meteorologicas favoraveis ou nÃ£o
 	private boolean condMeteo;
-	
-	// lista de prioridades de aeronaves em viagem para esta estação
+	// lista de prioridades de aeronaves em viagem para esta estaÃ§Ã£o
 	private List<String> listaPrioridade;
 	
-	// lista de espera para iniciar viagem para esta estação
-	private List<String> listpedidosChegada;
-	
-	// list de espera para iniciar viagem com saída nesta estação
-	private List<String> listpedidosSaida;
-	
-	private String name = "";
-	
-	
-	
-	@Override
 	protected void setup() {
-			
-			this.listaPrioridade = new ArrayList<>();
-			this.listpedidosSaida = new ArrayList<>();
-			this.listpedidosChegada = new ArrayList<>();
-			this.zonaProtegida = 2500;
-			this.zonaAlerta = 1000;
-			this.name = this.getLocalName();
-			this.totalPistas = 5;
-			this.nrPistasOcupadas = 0;
-			this.totalEstacionamentos = 5;
-			this.nrEstOcupados = 0;
-			
-	
-			
-			DFAgentDescription dfd = new DFAgentDescription();
-			dfd.setName(getAID());
-			ServiceDescription sd = new ServiceDescription();
-			sd.setName(getLocalName());
-			sd.setType(getLocalName());
-			dfd.addServices(sd);
-			try {
-				DFService.register(this, dfd);
-			} catch (FIPAException e) {
-				
-				e.printStackTrace();
-			}
-			
-			
-			initEstacao();
-			// 2 em 2 segs
-			this.addBehaviour(new verificarListasEspera(this, 2000));
-			this.addBehaviour(new receberPedidos());
-			
-		
+		super.setup();
+		this.listaPrioridade = new ArrayList<>();
+		this.zonaAlerta = 1000;
+		this.zonaProtegida = 100;
+		this.totalPistas = 5;
+		this.nrPistasOcupadas = 0;
+		this.totalEstacionamentos = 5;
+		this.nrEstOcupados = 0;
+		Object[] args = getArguments();
+		if (args != null && args.length>0) {
+			coordX=(double) args[0];
+			coordY=(double) args[1];
+			condMeteo=(boolean) args[2];
+		}
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setName(getLocalName());
+		sd.setType(getLocalName());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		this.addBehaviour(new receberPedidos());
 	}
-	
 	
 	private class receberPedidos extends CyclicBehaviour {
-		public void action() {
-			
+		public void action() {		
 			ACLMessage msg = receive();
-			
+			ACLMessage resp=null;
+			if(msg != null)
+				resp=msg.createReply();
 			if (msg != null && msg.getPerformative() == ACLMessage.REQUEST) {
-				
-				String aeronave = msg.getSender().getLocalName();
-				ACLMessage resp = msg.createReply();
-				String pedido = msg.getContent();
-				switch(pedido) {
-				// recebido de um AA que esteja neste aeroporto
-				case "PEDIDOSAIDA":
-					if(condMeteo && nrPistasOcupadas < totalPistas) {
-						nrEstOcupados--;
-						resp.setContent("AUTORIZA-SAIDA");
-						resp.setPerformative(ACLMessage.CONFIRM);
-						send(resp);
-						System.out.println(name +" autorizou uso da pista a " + aeronave);
-					}else {
-						// caso nao hajam cond no aeroporto origem é adicionado à lista de aeronaves para sair
-						listpedidosSaida.add(aeronave);
-						resp.setContent("NAO-AUTORIZA-SAIDA");
-						resp.setPerformative(ACLMessage.DISCONFIRM);
-						send(resp);
-						System.out.println(name +" não autorizou uso da pista a " + aeronave);
-					}
-				break;
-				//recebido de um AA que queira vir para este aeroporto
-				case "PEDIDOENTRADA":
-					if((nrEstOcupados < totalEstacionamentos) && condMeteo) {
-						nrEstOcupados++;
-						resp.setContent("AUTORIZA-ENTRADA");
-						resp.setPerformative(ACLMessage.CONFIRM);
-						send(resp);
-						System.out.println(name +" autorizou inicio de viagem a " + aeronave);
-					}else {
-						// caso nao hajam cond no aeroporto destino é adicionada à lista de aeronaves para chegar
-						listpedidosChegada.add(aeronave);
-						resp.setContent("NAO-AUTORIZA-ENTRADA");
-						resp.setPerformative(ACLMessage.DISCONFIRM);
-						send(resp);
-						System.out.println(name +" não autorizou inicio de viagem a " + aeronave);
-					}
-				break;
-				case "PEDIDOATERRAGEM":
-					 // verificar se é o primeiro na lista e diminuir a velocidade aos outros??
-		
-				
+				if(condMeteo && nrPistasOcupadas < totalPistas) {
+					nrEstOcupados--;
+					resp.setContent(coordX+";"+coordY);
+					resp.setPerformative(ACLMessage.CONFIRM);
+					send(resp);
 				}
-			}
-			if(msg != null && msg.getPerformative() == ACLMessage.INFORM) {
-				String aeronave = msg.getSender().getLocalName();
-				ACLMessage resp = msg.createReply();
-				String[] info = msg.getContent().split(";");
-				String content = msg.getContent();
-				switch(info[0]) {
-					case "INICIARVIAGEM":
-						listaPrioridade.add(content);
-						
-						//calculaPrioridade(); TODO-> Método para calculo da prioridade
-						
+			}else if(msg != null && msg.getPerformative() == ACLMessage.REQUEST_WHEN) {
+				if((nrEstOcupados < totalEstacionamentos) && condMeteo) {
+					nrEstOcupados++;
+					String[] coordsAviao = msg.getContent().split(";");
+					double aviaoCoordX=Double.parseDouble(coordsAviao[1]);
+					double aviaoCoordY=Double.parseDouble(coordsAviao[2]);
+					double vel=Double.parseDouble(coordsAviao[3]);
+					double distPercorrer=Math.sqrt(((Math.pow((aviaoCoordX - coordX), 2)) + (Math.pow((aviaoCoordY - coordY), 2))));
+					listaPrioridade=adicionaLista(distPercorrer/vel,coordsAviao[0]);
+					resp.setContent(coordX+";"+coordY+";"+zonaAlerta+";"+zonaProtegida+";"+distPercorrer);
+					resp.setPerformative(ACLMessage.AGREE);
+					send(resp);
 				}
-				
+			}else if(msg != null && msg.getPerformative() == ACLMessage.PROPOSE) {
+				resp.setContent(100+"");
+				resp.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+				double vel=zonaProtegida-5*getVelocidade(msg.getContent());
+				resp.setContent(vel+"");
+				send(resp);
+			}else if(msg != null && msg.getPerformative() == ACLMessage.INFORM) {
+				String n=msg.getContent();
+				listaPrioridade=removeLista(n);
+				nrEstOcupados--;
+				nrPistasOcupadas--;
+			}else if(msg != null && msg.getPerformative() == ACLMessage.INFORM_IF) {
+				String[] coordsAviao = msg.getContent().split(";");
+				double temp1=Double.parseDouble(coordsAviao[1]);
+				double temp2=Double.parseDouble(coordsAviao[2]);
+				listaPrioridade=removeLista(coordsAviao[0]);
+				listaPrioridade=adicionaLista(temp1+temp2,coordsAviao[0]);
 			}
 		}
 	}
 	
-	private class verificarListasEspera extends TickerBehaviour {
-		 public verificarListasEspera(Agent a, long tempo) {
-			 super(a,tempo);
-		 }
-		 
-		 @Override
-		 protected void onTick() {
-			 // se existirem aeronaves em espera iniciar partida desta estaçao e já existem condições para efetuar descolagem
-			 if(!listpedidosSaida.isEmpty() && (nrPistasOcupadas < totalPistas) && condMeteo) {
-				
-				try {
-					
-					
-					 // aceder às páginas amarelas para buscar o AID da aeronave
-					DFAgentDescription dfd = new DFAgentDescription();
-					ServiceDescription sd = new ServiceDescription();
-					sd.setType(listpedidosSaida.get(0));
-					dfd.addServices(sd);
-						
-					DFAgentDescription[] results = DFService.search(this.myAgent, dfd);
-					AID provider = results[0].getName();
-					 
-					if(results.length == 1) {
-						
-						ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
-						msg.addReceiver(provider);
-						msg.setContent("AUTORIZA-SAIDA");
-						send(msg);
-					}
-					
-				
-					listpedidosSaida.remove(0);
-				
-				 }catch(FIPAException e) {
-					 e.printStackTrace();
-				 }
-			 }
-			 
-			 // se existem aeronaves em espera para iniciar chegada a esta estaçao e já existem conds
-			 if(!listpedidosChegada.isEmpty() && (nrEstOcupados < totalEstacionamentos) && condMeteo) {
-				 
-				 try{
-				
-				 // aceder às páginas amarelas para buscar o AID da aeronave
-					DFAgentDescription dfd = new DFAgentDescription();
-					ServiceDescription sd = new ServiceDescription();
-					sd.setType(listpedidosChegada.get(0));
-					dfd.addServices(sd);
-						
-					DFAgentDescription[] results = DFService.search(this.myAgent, dfd);
-					AID provider = results[0].getName(); 
-					
-					if(results.length == 1) {
-						
-						ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
-						msg.addReceiver(provider);
-						msg.setContent("AUTORIZA-ENTRADA");
-						send(msg);
-					}
-					listpedidosChegada.remove(0);
-				
-				 }catch(FIPAException e) {
-					 e.printStackTrace();
-				 }
-			 }
+	private List<String> adicionaLista(double tempo, String name) {
+		double tempoAux=0.0;
+		if(listaPrioridade.size()==0)
+			listaPrioridade.add(0, name+";"+tempo);
+		else {
+			for(int i=0;i<listaPrioridade.size();i++) {
+				String[] infoAux = listaPrioridade.get(i).split(";");
+				tempoAux=Double.parseDouble(infoAux[1]);
+				if(tempo<tempoAux)
+					listaPrioridade.add(i, name+";"+tempo);
+			}
 		}
-					
+		return listaPrioridade;
 	}
 	
-	public void calculaPrioridade() {
-		
-	}
-		
-	
-
-	private void initEstacao() {
-		switch(this.getLocalName()) {
-		case "Estacao1":
-			coordX = -10000;
-			coordY = -500;
-			condMeteo = true;
-			
-		
-			break;
-			
-		case "Estacao2":
-			coordX = -10000;
-			coordY = 500;
-			condMeteo = true;
-			break;
-			
-		case "Estacao3":
-			coordX = 10000;
-			coordY = 0;
-			condMeteo = true;
-			break;
-		
-		case "Estacao4":
-			coordX = 0;
-			coordY = 11000;
-			condMeteo = false;
-		
+	private double getVelocidade(String name) {
+		double res=1.0;
+		for(int i=0;i<listaPrioridade.size();i++) {
+			String[] infoAux = listaPrioridade.get(i).split(";");
+			if(infoAux[0].equals(name)) {
+				res=i;
+				break;
+			}
 		}
+		return res;
 	}
 	
-	
-	
-	@Override
+	private List<String> removeLista(String name) {
+		for(int i=0;i<listaPrioridade.size();i++) {
+			String[] infoAux = listaPrioridade.get(i).split(";");
+			if(infoAux[0].equals(name)) {
+				listaPrioridade.remove(i);
+				break;
+			}
+		}
+		return listaPrioridade;
+	}
+		
 	protected void takeDown() {
-		super.takeDown();
-		
+		super.takeDown();	
 		try {
 			DFService.deregister(this);
 		}catch(FIPAException e) {
 			e.printStackTrace();
 		}
 	}
-
-
 }
